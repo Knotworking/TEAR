5f0241d5dfddadb0fdbf6a77a5d980797581e043
Ben Russell <ben@couchbits.com>
2021-08-21 12:45:16 +0200

    compose UI

--- app/src/main/java/com/knotworking/tear/MainActivity.kt
+++ app/src/main/java/com/knotworking/tear/MainActivity.kt
@@ -1,13 +1,19 @@
 package com.knotworking.tear
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import kotlinx.android.synthetic.main.activity_main.*
+import androidx.activity.compose.setContent
+import androidx.appcompat.app.AppCompatActivity
+import org.koin.android.viewmodel.ext.android.viewModel
 
 class MainActivity : AppCompatActivity() {
+
+    private val wordViewModel: WordViewModel by viewModel()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        setSupportActionBar(toolbar)
+        wordViewModel.requestNewWord()
+        setContent {
+            RandomWordContent(viewModel = wordViewModel)
+        }
     }
 }
