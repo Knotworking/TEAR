b35ba06839ad93dcfc11d6dd2f9aac4bd5bd758b
Ben Russell <ben@couchbits.com>
2021-09-10 15:35:22 +0200

    code cleanup

--- app/src/main/java/com/knotworking/tear/WordViewModel.kt
+++ app/src/main/java/com/knotworking/tear/WordViewModel.kt
@@ -7,11 +7,6 @@ import kotlinx.coroutines.flow.*
 internal class WordViewModel(
     private val getRandomWordUseCase: GetRandomWordUseCase
 ) : BaseViewModel() {
-
-    // TODO: how do the other projects do this
-//    val loading = MutableStateFlow(false)
-    val hasError = MutableStateFlow(false)
-
     val wordViewState: StateFlow<WordViewState>
         get() = _wordViewState
     private var _wordViewState = MutableStateFlow<WordViewState>(
@@ -20,17 +15,14 @@ internal class WordViewModel(
 
     override val coroutineExceptionHandler = CoroutineExceptionHandler { _, exception ->
         // handle error
-//        _paywallViewState.value = _paywallViewState.value.copy(error = Error(message = R.string.__error)) // TODO: map error message
-        hasError.value = true
+        _wordViewState.value = _wordViewState.value.copy(hasError = true)
     }
 
     fun requestNewWord() {
         launchInViewModelScope {
             getRandomWordUseCase(Unit).onStart {
-                hasError.value = false
                 _wordViewState.value = WordViewState(loading = true)
             }.catch {
-                hasError.value = true
                 _wordViewState.value = WordViewState(hasError = true)
             }.onCompletion {
             }.collect {
